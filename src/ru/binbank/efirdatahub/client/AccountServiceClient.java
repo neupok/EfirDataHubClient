61db17027d6d086e8a53c0913c73d5caac17c18d
Клеймёнов Владислав Владимирович <KleymenovV@corp.icba.biz>
2018-01-29 17:54:16 +0300

    updating DataHubClient.java

--- src/ru/binbank/efirdatahub/client/AccountServiceClient.java
+++ src/ru/binbank/efirdatahub/client/AccountServiceClient.java
@@ -3,6 +3,8 @@ package ru.binbank.efirdatahub.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.binbank.efirdatahub.entities.accountservice.contracts.LoginRequest;
 import ru.binbank.efirdatahub.entities.accountservice.contracts.LoginResponse;
+import ru.binbank.efirdatahub.entities.accountservice.contracts.LogoffRequest;
+import ru.binbank.efirdatahub.entities.accountservice.contracts.LogoffResponse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -13,9 +15,6 @@ import java.security.NoSuchAlgorithmException;
 public class AccountServiceClient extends DataHubClient {
     public AccountServiceClient(ClientConnectionSettings connectionSettings) {
         super(connectionSettings);
-        {}
-
-
     }
 
     public LoginResponse Login(LoginRequest loginRequest)
@@ -38,4 +37,29 @@ public class AccountServiceClient extends DataHubClient {
         }
         return loginResponse;
     }
+
+    public LogoffResponse Logoff(LogoffRequest logoffRequest)
+    {
+        LogoffResponse logoffResponse = null;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            // В результате вернётся строка, в которой будет:
+            // - если успех: пустой token;
+            // - если ошибка: Error, StatusCode, SubStatusCode;
+            String retString = postSync("/Account/Logoff", objectMapper.writeValueAsString(logoffRequest));
+            logoffResponse = objectMapper.readValue(retString, LogoffResponse.class);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return logoffResponse;
+    }
+
 }
