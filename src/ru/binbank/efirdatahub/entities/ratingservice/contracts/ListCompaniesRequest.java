3eefaf6664fc8776ec002526c90c439635954ab3
Клейменов Владислав <kleymenow@gmail.com>
2018-01-30 19:48:51 +0300

    neupokoev's changes

---
+++ src/ru/binbank/efirdatahub/entities/ratingservice/contracts/ListCompaniesRequest.java
@@ -0,0 +1,24 @@
+package entities.ratingservice.contracts;
+
+import java.util.HashMap;
+
+public class ListCompaniesRequest implements ru.binbank.efirdatahub.entities.IRequest {
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    private String filter;
+
+    @Override
+    public HashMap<String, String> getParams() {
+        HashMap<String, String> result = new HashMap<String, String>();
+        if (filter != null)
+            if (!filter.isEmpty())
+                result.put("filter", filter);
+        return result;
+    }
+}
