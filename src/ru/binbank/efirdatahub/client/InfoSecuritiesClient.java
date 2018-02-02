32e437cc64a0a78c228da4954dd6ba9d66164049
ESHILINA <des06392@mail.ru>
2018-02-02 16:22:13 +0300

    Обновлен класс InfoSecuritiesClient и созданы методы EmitentsRequest, FieldsRequest, FintoolFieldsRequest, InstrumentsRequest

--- src/ru/binbank/efirdatahub/client/InfoSecuritiesClient.java
+++ src/ru/binbank/efirdatahub/client/InfoSecuritiesClient.java
@@ -1,6 +1,9 @@
 package ru.binbank.efirdatahub.client;
 
 import ru.binbank.efirdatahub.entities.TableResponse;
+import ru.binbank.efirdatahub.entities.infoservice.contracts.EmitentsRequest;
+import ru.binbank.efirdatahub.entities.infoservice.contracts.FintoolFieldsRequest;
+import ru.binbank.efirdatahub.entities.infoservice.contracts.InstrumentsRequest;
 import ru.binbank.efirdatahub.entities.infoservice.contracts.SecuritiesRequest;
 
 import java.io.IOException;
@@ -16,5 +19,14 @@ public class InfoSecuritiesClient extends DataHubClient {
 
     public TableResponse Securities(SecuritiesRequest securitiesRequest) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
         return (TableResponse) runMethod("Info/Securities", "GET", securitiesRequest, TableResponse.class);
+    }
+    public TableResponse Emitents(EmitentsRequest emitentsRequest) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        return (TableResponse) runMethod("Info/Emitents", "GET", emitentsRequest, TableResponse.class);
+    }
+    public TableResponse Instruments(InstrumentsRequest instrumentsRequest) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        return (TableResponse) runMethod("Info/Instruments", "GET", instrumentsRequest, TableResponse.class);
+    }
+    public TableResponse FintoolFields(FintoolFieldsRequest fintoolFieldsRequest) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        return (TableResponse) runMethod("Info/FintoolFields", "GET", fintoolFieldsRequest, TableResponse.class);
     }
 }
